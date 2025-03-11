2921aaf619d0b459a6b8f51a3da5398d92540691
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-11 23:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => {
+const divide = (arr, v) => {
   // Write your code here
+	let sum =0;
+	let res = [];
+	let a = [];
+	let r = 0;
+    let n = arr.length;
+    
+	while(r<n){
+		sum += arr[r];
+		if(sum<=v){
+			a.push(arr[r]);
+			r++;
+		}
+		else{
+		    console.log(a);
+			res.push(a);
+			a = [];
+			a.push(arr[r]);
+			sum = arr[r];
+			r++;
+		}
+	}
+	
+	if(a.length>0){
+	    res.push(a);
+	}
+
+	return res;
+	
+	
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+const v = prompt("Enter v: ");
+alert(JSON.stringify(divide(arr, v)));
+
